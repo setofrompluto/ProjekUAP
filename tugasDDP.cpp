9b9f1430aff4bc1f526ce9cd3c20c7968f55abab
setofrompluto <53t0471@gmail.com>
2024-11-28 00:03:01 +0700

    Update tugasDDP.cpp
    finishing

--- tugasDDP.cpp
+++ tugasDDP.cpp
@@ -12,7 +12,30 @@ int main()
     noecho();
     keypad(stdscr, TRUE);
     curs_set(0);
-
+    
+    system("COLOR 0e");
+	system("cls");
+	printf("\e[?251");
+	
+	
+	SetConsoleCP(437);
+	SetConsoleOutputCP(437);
+	int bar1 = 177, bar2 = 219;
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\tLLoading...";
+	cout << "\n\n\n\t\t\t\t\t\t\t\t";
+	
+	for(int i = 0; i < 25; i++)
+		cout << (char)bar1;
+		
+		cout << "\r";
+		cout << "\t\t\t\t\t\t\t\t";
+		for(int i = 0; i < 25; i++){
+			cout << (char)bar2;
+			Sleep(150);
+		}
+		cout << "\n\t\t\t\t\t\t\t\t" << (char)1 << "!";
+		system("Pause");
+    
     string username;
 
     clear();
